72907d6893e3318d571757006c296d2e25409803
brooksphilip <brooksphilip@mac.com>
2023-12-29 13:53:12 -0500

    adding selinux

--- first_master_script.sh
+++ first_master_script.sh
@@ -2,10 +2,14 @@ echo "installing RKE2. If this fails check your repos that are enabled"
 curl -sfL https://get.rke2.io |  INSTALL_RKE2_VERSION="v1.25.16+rke2r1" sh -
 echo "enabling the rke2 service"
 systemctl enable rke2-server
+echo "setting up etcd user and applying sysctl"
+useradd -r -c "etcd user" -s /sbin/nologin -M etcd -U
+cp -f /usr/share/rke2/rke2-cis-sysctl.conf /etc/sysctl.d/60-rke2-cis.conf
+systemctl restart systemd-sysctl
 echo "setting up the rke2 config.yaml"
-mkdir -p /etc/rancher/rke2
+mkdir -p /etc/rancher/rke2/ /var/lib/rancher/rke2/server/manifests/
 echo "token: ${1}" > /etc/rancher/rke2/config.yaml
-echo "selinux: true" >> /etc/rancher/rke2/config.yaml
+echo -e "#profile: cis-1.23\nselinux: true\nsecrets-encryption: true\nwrite-kubeconfig-mode: 0600\nkube-controller-manager-arg:\n- bind-address=127.0.0.1\n- use-service-account-credentials=true\n- tls-min-version=VersionTLS12\n- tls-cipher-suites=TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384\nkube-scheduler-arg:\n- tls-min-version=VersionTLS12\n- tls-cipher-suites=TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384\nkube-apiserver-arg:\n- tls-min-version=VersionTLS12\n- tls-cipher-suites=TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384\n- authorization-mode=RBAC,Node\n- anonymous-auth=false\n- audit-policy-file=/etc/rancher/rke2/audit-policy.yaml\n- audit-log-mode=blocking-strict\n- audit-log-maxage=30\nkubelet-arg:\n- protect-kernel-defaults=true\n- read-only-port=0\n- authorization-mode=Webhook" >>/etc/rancher/rke2/config.yaml
 echo "setting up kubectl"
 echo 'PATH=${PATH}:/var/lib/rancher/rke2/bin' >> ~/.bashrc
 mkdir ~/.kube
