b20a8eefb456407d68e46fdfe9b679f0f79ce395
buskeyl <commerce@buskey.com>
2023-12-31 17:59:28 -0500

    Update install_rancher.sh
    Fixed a typo.

--- install_rancher.sh
+++ install_rancher.sh
@@ -3,5 +3,5 @@ helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 echo "creating cattle system namespace"
 kubectl create namespace cattle-system
 echo "installing Rancher"
-elm install rancher rancher-stable/rancher --namespace cattle-system --set hostname=rancher.my.org --set bootstrapPassword=admin --set global.cattle.psp.enabled=false
+helm install rancher rancher-stable/rancher --namespace cattle-system --set hostname=rancher.my.org --set bootstrapPassword=admin --set global.cattle.psp.enabled=false
 kubectl -n cattle-system rollout status deploy/rancher
